a507212d383a148883239fc6d19ac79fe4335c37
Your Name <bloodsportrecords@gmail.com>
2022-12-13 18:22:58 +0000

    add states example scripts2

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
 import './App.css';
+import FunctionalGreeting from './components/FunctionalGreeting';
+import StatefulGreeting from './components/StatefulGreeting';
+import HooksCounter from './components/HooksCounter';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* <FunctionalGreeting name="Trevor" /> */}
+      {/* <StatefulGreeting name="Trevor You Poes" /> */}
+      <HooksCounter name="Mike" />
     </div>
   );
 }
