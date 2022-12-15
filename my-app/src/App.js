f0973fd612930dd0ec197ca41b59eece6ab02c7a
Your Name <bloodsportrecords@gmail.com>
2022-12-15 12:41:35 +0000

    Create setState() and change values in StatefulGreeting.js

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,13 +2,15 @@ import './App.css';
 import FunctionalGreeting from './components/FunctionalGreeting';
 import StatefulGreeting from './components/StatefulGreeting';
 import HooksCounter from './components/HooksCounter';
+import StatefulGreetingTwo from './components/StatefulGreetingTwo';
 
 function App() {
   return (
     <div className="App">
       {/* <FunctionalGreeting name="Trevor" /> */}
       {/* <StatefulGreeting name="Trevor You Poes" /> */}
-      <HooksCounter name="Mike" />
+      {/* <HooksCounter name="Mike" /> */}
+      <StatefulGreetingTwo greeting="This is a message from the class component"/>
     </div>
   );
 }
---
+++ my-app/src/components/StatefulGreetingTwo.js
@@ -0,0 +1,43 @@
+import React from "react";
+
+// This is the correct way to create the class
+class StatefulGreetingTwo extends React.Component {
+    // The constructor is usually where state is defined in class components
+    // it is best practice to always pass props to the constructor() and super()
+    constructor(props) {
+        super(props);
+        // as soon as its instantiated that we can access and manipulate as required
+        this.state = {
+            // Changeable Properties, referred to as the initial state and can change based on updating or user interactivity
+            introduction: "Hello",
+            buttonText: "Exit",
+        };
+    };
+
+    // This is connected to the <button> and is called to update the state
+    handleClick() {
+        // You need to write this syntax to successfully update the state, or you will get errors
+        this.setState({
+            // We are replacing this from the this.state = {
+                // =====================
+                // introduction: "Hello",
+                // buttonText: "Exit",
+                // =====================
+            // }
+            introduction: "Goodbye!",
+            buttonText: "Enter!",
+        });
+        console.log(this.state.introduction)
+    }
+
+    render() {
+        return (
+            <div>
+                <h1>{ this.state.introduction }, { this.props.greeting }</h1>
+                <button onClick={() => this.handleClick()}>{ this.state.buttonText }</button>
+            </div>
+        )
+    };
+};
+
+export default StatefulGreetingTwo;
